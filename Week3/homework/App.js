7f30e55cff29e4e5e049b4cb9d087b56ba2072d7
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-21 23:14:39 +0530

    Style added with react app

--- Week3/homework/App.js
+++ Week3/homework/App.js
@@ -1,16 +1,16 @@
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className = "MainBox">
                 <Hello
                     from  = "Ankit"
                     to    = "Raja"
                     bangs = {4}
-                    url= "https://wallpapercave.com/wp/NGD9aMj.jpg"
+                    url= "https://i.pinimg.com/originals/3e/a1/37/3ea1371e08a4634823d8fbfa2ae14f65.jpg"
                 />
                 <Hello
                     from    = "Riki"
-                    to      = "Ridhi"
+                    to      = "Guddu"
                     bangs   = {10}
                     url= 'https://downloadwallpapers-net.s3.amazonaws.com/blue-night-and-moon-nature-hd-wallpapers.jpg'
                 />
--- Week3/homework/Hello.js
+++ Week3/homework/Hello.js
@@ -3,9 +3,9 @@ class Hello extends React.Component{
         let bangs = "!".repeat(this.props.bangs);
         {/*console.log(this.props);*/}
         return (
-            <div>
-                <p>Hello {this.props.to} from {this.props.from}{bangs}</p>
-                <img src = {this.props.url} height = "400px" />
+            <div style = {{margin : "30px 0"}}>
+                <p style = {{textAlign:"center" , display : "block" , fontSize : "24px" , margin:"0 0 30px 0"}}>Hello {this.props.to} from {this.props.from}{bangs}</p>
+                <img style={{width:"100%" ,position:"relative" }} src = {this.props.url} />
             </div>
         );
     }
