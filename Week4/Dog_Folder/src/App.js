33f75f3b7c8c58720f9d04113481df1b48d0cb4f
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-23 00:03:08 +0530

    React with CSS done

---
+++ Week4/Dog_Folder/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+import Comp from "./Component";
+import './App.css';
+
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Comp />
+      </div>
+    );
+  }
+}
+
+export default App;
