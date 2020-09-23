e64d5b39646b6161f2bf60206b248a7f385f1110
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-23 20:27:41 +0530

    exercise done for state

--- Week5/basic2/src/App.js
+++ Week5/basic2/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      isWin: false,
+      num  : 1
+    }
+    this.newNumber = this.newNumber.bind(this);
+  }
+
+  newNumber(e) {
+    let rand = Math.floor(Math.random() * 10);
+    this.setState({ num: rand });
+    if (rand === 7) {
+      this.setState({
+        isWin : true,
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Current Number is : {this.state.num}</h1>
+        {this.state.isWin ? <p>You win the game</p> : <button onClick = {this.newNumber}>New Number</button>}
+      </div>
+    );
+  }
 }
 
 export default App;
