2ac81cb82db400dadb4680788f8850d323a0e10b
Ankit Podder <ankitpodder0211@gmail.com>
2020-09-28 00:12:47 +0530

    done , few left style

--- week9/basic2/src/HangmanGame.js
+++ week9/basic2/src/HangmanGame.js
@@ -1,105 +1,123 @@
 import React, { Component } from "react";
 import HangmanImg from "./HangmanImg";
-import choose_word from "./chooseWord";
+import words from "./chooseWord";
 import "./HangmanGame.css";
 
-class HangmanGame extends Component{
-    static defaultProps = {
-        word: choose_word,
-        keys : "abcdefghijklmnopqrstuvwxyz".split("")
-    }
-    makeInitialWord() {
-        let word = "";
-        for (let i = 0; i < this.props.word.length; i++){
-            word += "*";
-        }
-        return word;
-    }
-    constructor(props) {
-        super(props);
-        this.state = {
-            img: 10 - this.props.word.length,
-            currentWord: this.makeInitialWord(),
-            leftChance: this.props.word.length - 1,
-            win : false
-        }
-    }
+class HangmanGame extends Component {
+  static defaultProps = {
+    keys: "abcdefghijklmnopqrstuvwxyz".split(""),
+  };
 
-    makeNewCurrentWord(cur_st , ch) {
-        let currentWord = cur_st.currentWord;
-        let newCurrentWord = "";
-        for (let i = 0; i < this.props.word.length; i++){
-            if (this.props.word[i] === ch) {
-                newCurrentWord += ch;
-            } else {
-                newCurrentWord += currentWord[i];
-            }
-        }
-        for (let i = 0; i < newCurrentWord.length; i++){
-            if (newCurrentWord[i] === '*') {
-                return { currentWord: newCurrentWord };
-            }
-        }
-        return { currentWord: newCurrentWord , win : true};
-        
+  makeInitialWord(Word) {
+    let word = "";
+    for (let i = 0; i < Word.length; i++) {
+      word += "*";
     }
-    
-    genID() {
-        return Math.floor(Math.random() * 10000);
-    }
+    return word;
+  }
 
-    click(e , ch) {
-        if (this.state.win || this.state.leftChance === -1)
-            return;
-        e.classList.add("deactive");
-        e.disabled = true;
-        if (!this.props.word.match(ch)) {
-            this.setState(currentState => {
-                return {
-                    leftChance: currentState.leftChance - 1,
-                    img : currentState.img + 1
-                }
-            })
-            return;
-        } else {
-            this.setState(currentState => this.makeNewCurrentWord(currentState, ch));
-        }
-    }
+  constructor(props) {
+    super(props);
+    let word = words[Math.floor(Math.random() * words.length)];
+    this.state = {
+      word: word,
+      img: 10 - word.length,
+      currentWord: this.makeInitialWord(word),
+      leftChance: word.length - 1,
+      win: false,
+      guess : new Set(),
+    };
+  }
+  restart(){
+      let word = words[Math.floor(Math.random() * words.length)];
+      this.setState({
+        word: word,
+        img: 10 - word.length,
+        currentWord: this.makeInitialWord(word),
+        leftChance: word.length - 1,
+        win: false,
+        guess : new Set(),
+      });
+  }
 
-    render() {
-        let arr = [];
-        for (let i = 0; i < this.state.currentWord.length; i++){
-            
-            if (this.state.currentWord[i] === "*" && this.state.leftChance===-1) {
-                arr.push(<p className="red">{this.props.word[i]}</p>);
-            } else {
-                arr.push(<p>{this.state.currentWord[i]}</p>);
-            }
-        }
-        return (
-          <div className="hangmangame">
-            <HangmanImg img_num={this.state.img} />
-            <p className="msg">
-              {this.state.leftChance === -1
-                ? "Game Over!"
-                : this.state.win
-                ? "Woww , You win!"
-                : `Guess left : ${this.state.leftChance}`}
-            </p>
-            <div className="text">
-              {arr}
-            </div>
-            <div className="keyboard">
-              {this.props.keys.map((k) => (
-                <button onClick={(e) => this.click(e.target , k)}>{k}</button>
-              ))}
-            </div>
-            <button className="restart">
-                {this.state.leftChance === -1 ? "Play Again ? " : this.state.win ? "Play Again ?" : "Restart ?"}
-            </button>
-          </div>
-        );
+  makeNewCurrentWord(cur_st, ch) {
+    let currentWord = cur_st.currentWord;
+    let newCurrentWord = "";
+    for (let i = 0; i < this.state.word.length; i++) {
+      if (this.state.word[i] === ch) {
+        newCurrentWord += ch;
+      } else {
+        newCurrentWord += currentWord[i];
+      }
     }
+    for (let i = 0; i < newCurrentWord.length; i++) {
+      if (newCurrentWord[i] === "*") {
+        return {
+          currentWord: newCurrentWord,
+          guess: cur_st.guess.add(ch),
+        };
+      }
+    }
+    return {
+      currentWord: newCurrentWord,
+      win: true,
+      guess: cur_st.guess.add(ch)
+    };
+  }
+
+  click(e, ch) {
+    if (this.state.win || this.state.leftChance === -1) return;
+    if (!this.state.word.match(ch)) {
+      this.setState((currentState) => {
+        return {
+          leftChance: currentState.leftChance - 1,
+          img: currentState.img + 1,
+          guess: currentState.guess.add(ch),
+        };
+      });
+      return;
+    } else {
+      this.setState((currentState) =>
+        this.makeNewCurrentWord(currentState, ch)
+      );
+    }
+  }
+
+  render() {
+    let arr = [];
+    for (let i = 0; i < this.state.currentWord.length; i++) {
+      if (this.state.currentWord[i] === "*" && this.state.leftChance === -1) {
+        arr.push(<p className="red">{this.state.word[i]}</p>);
+      } else {
+        arr.push(<p>{this.state.currentWord[i]}</p>);
+      }
+    }
+    return (
+      <div className="hangmangame">
+        <HangmanImg img_num={this.state.img} />
+        <p className="msg">
+          {this.state.leftChance === -1
+            ? "Game Over!"
+            : this.state.win
+            ? "Woww , You win!"
+            : `Guess left : ${this.state.leftChance}`}
+        </p>
+        <div className="text">{arr}</div>
+        <div className="keyboard">
+          {this.props.keys.map((k) => (
+            <button onClick={(e) => this.click(e.target, k)} disabled={this.state.guess.has(k)}>{k}</button>
+          ))}
+        </div>
+        <button className="restart" onClick={() => this.restart()}>
+          {this.state.leftChance === -1
+            ? "Play Again ? "
+            : this.state.win
+            ? "Play Again ?"
+            : "Restart ?"}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default HangmanGame;
