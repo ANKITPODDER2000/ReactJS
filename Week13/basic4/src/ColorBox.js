a06ecec3de7ff5e626bc8150214ed628fc1b02ec
Ankit Podder <ankitpodder0211@gmail.com>
2020-10-23 12:25:00 +0530

    Style colorbox

--- Week13/basic4/src/ColorBox.js
+++ Week13/basic4/src/ColorBox.js
@@ -3,9 +3,17 @@ import "./stylesheet/ColorBox.css";
 
 class ColorBox extends Component {
     render() {
+        const { background, name } = this.props;
+        console.log(background,name)
         return (
-            <div className="ColorBox" style={{background:this.props.background}}>
-                <span>{this.props.name}</span>
+            <div className="ColorBox" style={{background:background}}>
+                <div className="copy-container">
+                    <div className="box-content">
+                        <span>{name}</span>
+                    </div>
+                    <button className="copy-btn">Copy</button>
+                </div>
+                <span className="see-more">More</span>
             </div>
         );
     }
