c23cca0fb16f3b92926d4bd8a20759a5943cf972
Ankit Podder <ankitpodder0211@gmail.com>
2020-10-23 18:04:56 +0530

    Snackbar add & style add

--- Week13/basic4/src/Nav.js
+++ Week13/basic4/src/Nav.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./stylesheet/Nav.css";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 
 class Nav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            format: "hex",
+            open: false
+        }
+        this.handleFormat_ = this.handleFormat_.bind(this);
+        this.close = this.close.bind(this);
+    }
+    handleFormat_(evt) {
+        this.setState({
+            format: evt.target.value,
+            open : true
+        } , ()=> console.log(this.state))
+        this.props.handleFormat(evt);
+    }
+
+    close() {
+        this.setState({
+            open : false,
+        })
+    }
     render() {
         return (
             <nav>
@@ -22,6 +49,36 @@ class Nav extends Component {
                         onAfterChange = {this.props.handleChange}
                     />
                 </div>
+                <Select value={this.state.format} onChange={this.handleFormat_}>
+                    <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                    <MenuItem value="rgb">rgb - rgb(255 , 255 , 255)</MenuItem>
+                    <MenuItem value="rgba">rgba - rgba(255 , 255 , 255 ,1)</MenuItem>
+                </Select>
+                <Snackbar
+                    anchorOrigin={{vertical:"bottom" , horizontal:"left"}}
+                    open={this.state.open}
+                    message={
+                        <span id="message-id">Format Changed!</span>
+                    }
+                    ContentProps={{
+                        "aria-describedby":'message-id'
+                    }}
+                    onClose={() => {
+                        setTimeout(() => {
+                            this.close()
+                        } , 3000)
+                    }}
+                    action={[
+                        <IconButton
+                            onClick={this.close}
+                            color="inherit"
+                            key="close"
+                            aria-label="close"
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    ]}
+                />
             </nav>
         );
     }
--- Week13/basic4/src/Palette.js
+++ Week13/basic4/src/Palette.js
@@ -8,23 +8,34 @@ class Palette extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            value : 500
+            value: 500,
+            format : 'hex'
         }
         this.handleChange = this.handleChange.bind(this);
+        this.handleFormat = this.handleFormat.bind(this);
     }
     handleChange(level) {
         this.setState({
             value : level
         })
     }
+    handleFormat(evt) {
+        this.setState({
+            format : evt.target.value
+        })
+    }
     render() {
         let colorbox = this.props.colors[this.state.value].map(color =>
-            <ColorBox key={uuid()} background={color.hex} name={color.name}/>
+            <ColorBox key={uuid()} background={color[this.state.format]} name={color.name}/>
         );
         return (
             <div className="Palette">
                 {/* Navbar goes here */}
-                <Nav value={this.state.value} handleChange={this.handleChange}/>
+                <Nav
+                    value={this.state.value}
+                    handleChange={this.handleChange}
+                    handleFormat={this.handleFormat}
+                />
                 <div className="Palette-colors">
                     {/* ColorBox goes here */}
                     {colorbox}
