1df6cfa5d3bb783e292ddfb6db50f850be21c4aa
Ankit Podder <ankitpodder0211@gmail.com>
2020-11-05 12:57:53 +0530

    firebase cred add , style change off app comp

--- Week13/basic5/src/App.js
+++ Week13/basic5/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    render() {
+        return (
+            <div className="app">
+                <h1>Lets create a whatsapp clone</h1>
+                <div className="app_body"></div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ Week13/basic5/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRI6sMGYpwrBzXIpaBxppa4lLy2tcrTsU",
+  authDomain: "chat-system-63d06.firebaseapp.com",
+  databaseURL: "https://chat-system-63d06.firebaseio.com",
+  projectId: "chat-system-63d06",
+  storageBucket: "chat-system-63d06.appspot.com",
+  messagingSenderId: "710523291768",
+  appId: "1:710523291768:web:f58caba5d94ca6e579858d",
+  measurementId: "G-0QVKZ8QK9L",
+};
